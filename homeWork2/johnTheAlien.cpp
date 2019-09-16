edcd58ef7a0fbddfc3ee1e68a5f3ccedbea16ce0
RobMin <robertminasyan01@gmail.com>
2019-09-16 09:38:10 +0400

    review fixes

--- homeWork2/johnTheAlien.cpp
+++ homeWork2/johnTheAlien.cpp
@@ -3,7 +3,7 @@
 int main() {
     int r1, r2, r3;
     std::cin >> r1 >> r2 >> r3;
-    if (r2 * 2 + r3 * 2 <= r1) {
+    if (r2 + r3 <= r1) {
         std::cout << "YES";
     } else {
         std::cout << "NO";
